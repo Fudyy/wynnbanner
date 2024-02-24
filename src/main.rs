06f9686ac7b2ce7c987a7f5268af911a92acbcf7
Carlos González <104286054+Fudyy@users.noreply.github.com>
2024-02-24 18:42:13 -0300

    add: Json deserialization

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
-use actix_web::{get, App, HttpServer};
+use actix_web::{get, web, App, HttpServer, Responder};
+use serde::{Serialize, Deserialize};
 
 const PORT: &str = "5003";
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Pattern {
+    pattern: String,
+    color: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct RequestBody {
+    base_color: String,
+    patterns: Vec<Pattern>,
+}
+
 #[get("/")]
-async fn index() -> &'static str {
-    "Hello, world!"
+async fn index(request: web::Json<RequestBody>) -> impl Responder {
+    let mut result = String::from("Base color: ");
+    result.push_str(&request.base_color);
+    result.push_str("\nPatterns: ");
+    for pattern in &request.patterns {
+        result.push_str(&format!("{} - {} ", pattern.pattern, pattern.color));
+    }
+    result
 }
 
 #[actix_web::main]
