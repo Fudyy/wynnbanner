c39c07977e8ca2b11480a95b2baa4551ba16540f
Carlos González <104286054+Fudyy@users.noreply.github.com>
2024-02-24 18:22:47 -0300

    add: actixweb hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer};
+
+const PORT: &str = "5003";
+
+#[get("/")]
+async fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+        .service(index)
+    }).bind(format!("127.0.0.1:{}", PORT))?
+    .run()
+    .await
 }
